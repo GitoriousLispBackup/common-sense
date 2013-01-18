81877d1271e33dde8bb929f1939816fc4a95e648
Gordon Quad <gordon@niflheim.info>
2013-01-18 20:07:16 +0400

    Url methods boilerpated

--- src/url.lisp
+++ src/url.lisp
@@ -27,41 +27,208 @@
   (:qt-superclass "QUrl"))
 
 (defmethod initialize-instance :after ((instance url)
-                                       &key parent)
-  (if parent
-      (new instance parent)
+                                       &key value user-input)
+  (if value
+      (if user-input
+          (new instance value)
+          (new instance value))
       (new instance)))
-
-(defgeneric supported-content-types (ww)
-  (:documentation "")
-  (:method ((ww web-widget))
-    (#_supportedContentTypes (page ww))))
 
 (defgeneric error-string (url)
   (:documentation "")
   (:method ((url url))
     (#_errorString url)))
 
-errorString
-fromUserInput
-fragment/setFragment
-hasFragment
-hasQuery
-hasQueryItem
-host/setHost
-isEmpty
-isLocalFile
-isParentOf
-isValid
-isRelative
-password/setPassword
-path/setPath
-port/setPort
-scheme/setScheme
-queryItemValue
-removeAllQueryItems
-resolved
-authory/setAuthory
-userinfo/setUserinfo
-userName/setUserName
-url/setUrl
+(defgeneric empty-p (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_isEmpty url)))
+
+(defgeneric local-file-p (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_isLocalFile url)))
+
+(defgeneric parent-p (url child-url)
+  (:documentation "")
+  (:method ((url url) child-url)
+    (#_isParentOf url child-url)))
+
+(defgeneric valid-p (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_isValid url)))
+
+(defgeneric relative-p (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_isRelative url)))
+
+(defgeneric has-fragment-p (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_hasFragment url)))
+
+(defgeneric has-query-p (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_hasQuery url)))
+
+(defgeneric has-query-item-p (url item)
+  (:documentation "")
+  (:method ((url url) item)
+    (#_hasQueryItem url item)))
+
+(defgeneric resolved (url)
+  (:documentation "")
+  (:method ((url url))
+    (make-instance 'url :pointer (qt::qobject-pointer (#_resolved url)))))
+
+;; queryItems waiting for QPair marshalling
+;; Encoded
+
+(defgeneric remove-all-query-items (url item &key encoded)
+  (:documentation "")
+  (:method ((url url) item &key encoded)
+    (if encoded
+        (#_removeEncodedAllQueryItems url item)
+        (#_removeAllQueryItems url item))))
+
+(defgeneric query-item-value (url item &key encoded)
+  (:documentation "")
+  (:method ((url url) item &key encoded)
+    (if encoded
+        (#_data (#_encodedQueryItemValue url item))
+        (#_queryItemValue url item))))
+
+(defgeneric encoded-query (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_data (#_encodedQuery url))))
+
+(defgeneric host (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_encodedHost url))
+        (#_host url))))
+
+(defgeneric (setf host) (value url &key encoded)
+  (:documentation "")
+  (:method (value (url url) &key encoded)
+    (if encoded
+        (#_setEncodedHost url value)
+        (#_setHost url value))))
+
+(defgeneric fragment (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_encodedFragment url))
+        (#_fragment url))))
+
+(defgeneric (setf fragment) (value url &key encoded)
+  (:documentation "")
+  (:method (value (url url) &key encoded)
+    (if encoded
+        (#_setEncodedFragment url value)
+        (#_setFragment url value))))
+
+(defgeneric password (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_encodedPassword url))
+        (#_password url))))
+
+(defgeneric (setf password) (value url &key encoded)
+  (:documentation "")
+  (:method (value (url url) &key encoded)
+    (if encoded
+        (#_setEncodedPassword url value)
+        (#_setPassword url value))))
+
+(defgeneric path (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_encodedPath url))
+        (#_path url))))
+
+(defgeneric (setf path) (value url &key encoded)
+  (:documentation "")
+  (:method (value (url url) &key encoded)
+    (if encoded
+        (#_setEncodedPath url value)
+        (#_setPath url value))))
+
+(defgeneric port (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_encodedPort url))
+        (#_port url))))
+
+(defgeneric (setf port) (value url)
+  (:documentation "")
+  (:method (value (url url))
+    (#_setPort url value)))
+
+(defgeneric scheme (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_scheme url)))
+
+(defgeneric (setf scheme) (value url)
+  (:documentation "")
+  (:method (value (url url))
+    (#_setScheme url value)))
+
+(defgeneric authority (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_authority url)))
+
+(defgeneric (setf authority) (value url)
+  (:documentation "")
+  (:method (value (url url))
+    (#_setAuthority url value)))
+
+(defgeneric user-info (url)
+  (:documentation "")
+  (:method ((url url))
+    (#_userInfo url)))
+
+(defgeneric (setf user-info) (value url)
+  (:documentation "")
+  (:method (value (url url))
+    (#_setUserInfo url value)))
+
+(defgeneric user-name (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_encodedUserName url))
+        (#_userName url))))
+
+(defgeneric (setf user-name) (value url &key encoded)
+  (:documentation "")
+  (:method (value (url url) &key encoded)
+    (if encoded
+        (#_setEncodedUserName url value)
+        (#_setUserName url value))))
+
+(defgeneric url (url &key encoded)
+  (:documentation "")
+  (:method ((url url) &key encoded)
+    (if encoded
+        (#_data (#_toEncoded url))
+        (#_toString url))))
+
+(defgeneric (setf url) (value url &key encoded)
+  (:documentation "")
+  (:method (value (url url) &key encoded)
+    (if encoded
+        (#_setEncodedUrl url value)
+        (#_setUrl url value))))
+
